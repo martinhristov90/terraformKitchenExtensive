78f8f049152d7f3b412b81c74177ab65574e7c19
Martin Hristov <martinhristov@Martins-MacBook-Pro.local>
2019-06-10 14:39:17 +0300

    fixing intial content

--- main.tf
+++ main.tf
@@ -5,11 +5,10 @@ terraform {
 }
 
 provider "aws" {
-  version = "~> 1.31"
+  version    = "~> 1.31"
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
   region     = "${var.region}"
-  
 }
 
 provider "random" {
