bc4dd1e5caa52dadc136d894a54b7ad3ea0bc311
Martin Hristov <martinhristov@Martins-MacBook-Pro.local>
2019-06-10 14:37:12 +0300

    adding initial content

---
+++ test/fixtures/wrapper/main.tf
@@ -0,0 +1,13 @@
+module "extensive_kitchen_terraform" {
+  instances_ami = "${var.instances_ami}"
+
+  # The generated key pair will be used to configure SSH authentication
+  key_pair_public_key = "${file("${path.module}/../../assets/key_pair.pub")}"
+
+  # The source of the module is the root directory of the Terraform project
+  source                   = "../../../"
+  subnet_availability_zone = "${var.subnet_availability_zone}"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
